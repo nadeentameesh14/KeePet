79ca06097e161dd4596fc3ed22ad4d602ac13514
Nadeen Tameesh <nadeentameesh14@aucegypt.edu>
2020-01-19 01:33:44 +0200

    finished add post

--- app/src/main/java/com/example/myapplication/AddActivity.java
+++ app/src/main/java/com/example/myapplication/AddActivity.java
@@ -6,15 +6,91 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class AddActivity extends AppCompatActivity {
 
+    private Spinner typeSpin;
+    private Spinner breedSpin;
+    private Spinner genderSpin;
+
+    private ArrayAdapter<CharSequence> adapter;
+    private ArrayAdapter<CharSequence> adapter2;
+    private ArrayAdapter<CharSequence> adapter3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        typeSpin = (Spinner)findViewById(R.id.type_spinner);
+        breedSpin =(Spinner)findViewById(R.id.breed_spinner);
+        genderSpin =(Spinner)findViewById(R.id.gender_spinner);
+
+
+        adapter = ArrayAdapter.createFromResource(this,R.array.pet_types,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        typeSpin.setAdapter(adapter);
+
+        adapter3 = ArrayAdapter.createFromResource(this,R.array.genders,android.R.layout.simple_spinner_item);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        genderSpin.setAdapter(adapter3);
+
+        typeSpin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String text = parent.getItemAtPosition(position).toString();
+
+                switch (text) {
+                    case "Cat": {
+                        adapter2 = ArrayAdapter.createFromResource(getApplicationContext(),R.array.cat_breeds,android.R.layout.simple_spinner_item);
+                        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_item);
+                        breedSpin.setAdapter(adapter2);
+                    } break;
+                    case "Dog": {
+                        adapter2 = ArrayAdapter.createFromResource(getApplicationContext(),R.array.dog_breeds,android.R.layout.simple_spinner_item);
+                        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_item);
+                         breedSpin.setAdapter(adapter2);
+                    } break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        breedSpin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        genderSpin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
 
