a50348718f7929cb713750c7bc44abfd48c27d0d
Abdelrahman Moussa <abdulrahman.ibrahim@aucegypt.edu>
2020-01-20 16:03:53 +0200

    added api endpoints

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,20 @@
+// const router = require('express').Router()
+// const auth = require('./auth/auth')
+
+// router.use('/auth', auth)
+
+// module.exports = router
+
+
 const express = require('express')
 const mysql = require('mysql')
 const app = express();
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: false })); // support encoded bodies
+
 
 //create connection
 const db = mysql.createConnection({
@@ -38,15 +52,18 @@ app.post('/auth/login', function(request, response) {
 });
 
 
-app.get("/auth/register", function (req, res) {
+app.post("/auth/register", function (req, res) {
+
+    if( !req.body )
+      return res.sendStatus(400);
+
     // q = url.parse(req.url, true).query;
     let sql = "INSERT INTO user (email , first , last , password , phone ) VALUES (" ;
     sql = sql + "'" + req.body.email + "','" + req.body.first + "','" + req.body.last + "','" + req.body.password + "'," + req.body.phone +  ")";
     let query = db.query(sql, function (err, result) {
       if (err) {
         console.log(err);
-        console.log( req.body.email ) ;
-        res.send(err);
+        res.send(req);
         throw err;
       }
       else{
@@ -54,14 +71,17 @@ app.get("/auth/register", function (req, res) {
         console.log("user inserted sucessfully")
       };
     });
-  });
+});
 
 
 
-app.get("/createPet", function (req, res) {
-  // q = url.parse(req.url, true).query;
-  let sql = "INSERT INTO keepet (name, age , breed , type , gender , seller ) VALUES (" ;
-  sql = sql + req.body.name + "," + req.body.age + "," + req.body.breed + "," + req.body.type + "," + req.body.gender + "," + req.body.seller + ")";
+app.post("/pet/create",  function (req, res) {
+  
+    if( !req.body )
+      return res.sendStatus(400);
+
+  let sql = "INSERT INTO pet (name, age , breed , type , gender , seller , description , vaccination  ) VALUES (" ;
+  sql = sql + "'" + req.body.name + "'," + req.body.age + ",'" + req.body.breed + "','" + req.body.type + "','" + req.body.gender + "','" + "moussa"  + "','" +  req.body.description  + "'," +  req.body.vaccination + ")";
   let query = db.query(sql, function (err, result) {
     if (err) {
       console.log(err);
@@ -75,10 +95,14 @@ app.get("/createPet", function (req, res) {
   });
 });
 
-app.get("/getSMS", function (req, res) {
-  // q = url.parse(req.url, true).query;
-  let sql = "SELECT * FROM sms WHERE sent = 0 " ;
-  sql = sql + "ORDER BY id DESC LIMIT 1" ;
+
+app.get("/pet/get", function (req, res) {
+  
+  if( !req.body )
+      return res.sendStatus(400);
+
+  let sql = "SELECT * FROM pet WHERE id = " ;
+  sql = sql + req.query.id ;
   let query = db.query(sql, function (err, result) {
     if (err) {
       console.log(err);
@@ -91,21 +115,21 @@ app.get("/getSMS", function (req, res) {
   });
 });
 
-app.get("/sentSMS", function (req, res) {
-  // q = url.parse(req.url, true).query;
-  let sql = "UPDATE sms SET sent = 1 WHERE id =" ;
-  sql = sql + req.query.id ;
-  let query = db.query(sql, function (err, result) {
-    if (err) {
-      console.log(err);
-      throw err;
-    }
-    else{
-      res.send(result);
-      console.log("1 record changed")
-    };
-  });
-});
+// app.get("/sentSMS", function (req, res) {
+//   // q = url.parse(req.url, true).query;
+//   let sql = "UPDATE sms SET sent = 1 WHERE id =" ;
+//   sql = sql + req.query.id ;
+//   let query = db.query(sql, function (err, result) {
+//     if (err) {
+//       console.log(err);
+//       throw err;
+//     }
+//     else{
+//       res.send(result);
+//       console.log("1 record changed")
+//     };
+//   });
+// });
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
