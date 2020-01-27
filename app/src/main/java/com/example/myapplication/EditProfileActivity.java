9626f147c2da4a22163be19ccba88f4b33f58d2a
Abdelrahman Moussa <abdulrahman.ibrahim@aucegypt.edu>
2020-01-27 07:10:01 +0200

    whatever

--- app/src/main/java/com/example/myapplication/EditProfileActivity.java
+++ app/src/main/java/com/example/myapplication/EditProfileActivity.java
@@ -131,6 +131,7 @@ public class EditProfileActivity extends AppCompatActivity {
         startActivityForResult(intent,IMG_REQUEST);
     }
 
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -318,8 +319,6 @@ public class EditProfileActivity extends AppCompatActivity {
         };
 
         requestQueue.add(stringRequest);
-
-
     }
 
     public String imageToString(Bitmap bitmap) {
